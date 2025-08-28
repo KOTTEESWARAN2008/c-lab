fd0b0a7806a36836c472bf8a1755d2c2009b1b4f
KOTTEESWARAN2008 <ramusvc957@gmail.com>
2025-08-28 01:00:27 -0700

    Add files via upload

---
+++ kottee.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+
+{
+
+	printf("\naddition of a+b=%d",5+8);
+	printf("\nsubtraction of a-b=%d",5-8);
+	printf("\nMultiplication of a*b=%d",5*8);
+	printf("\nDivision of a/b=%d",5/5);
+	getch();
+}
